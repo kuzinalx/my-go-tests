07d92d07e32d559d54735a1ff77ab8637a5ab159
kuzinalx <kuzin_aleksei@mail.ru>
2022-03-07 13:13:33 +0300

    jpegsender: image displaying in fyne.Window added

--- src/jpegsender/main.go
+++ src/jpegsender/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/jpeg"
 	"log"
 	"mime/multipart"
 	"net/http"
@@ -13,6 +16,9 @@ import (
 	"sync"
 	"time"
 
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"github.com/gorilla/websocket"
 )
 
@@ -41,41 +47,72 @@ type FrameDataDecoded struct {
 }
 
 var frameDataDecoded FrameDataDecoded
+var window *fyne.Window
 
 func main() {
 	interrupt = make(chan os.Signal)
 	signal.Notify(interrupt, os.Interrupt)
 	go startWebsocketClient()
+	go showFrames()
 	//<-interrupt
 	//log.Println("Received SIGINT interrupt signal.")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		mpw := multipart.NewWriter(w)
-		w.Header().Add("Content-Type",
-			fmt.Sprintf("multipart/x-mixed-replace; boundary=\"%v\"", mpw.Boundary()))
-		//w.Header().Add("Content-Type", mpw.FormDataContentType())
-		w.WriteHeader(http.StatusOK)
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			mpw := multipart.NewWriter(w)
+			w.Header().Add("Content-Type",
+				fmt.Sprintf("multipart/x-mixed-replace; boundary=\"%v\"", mpw.Boundary()))
+			//w.Header().Add("Content-Type", mpw.FormDataContentType())
+			w.WriteHeader(http.StatusOK)
 
-		partHdr := textproto.MIMEHeader(map[string][]string{})
-		partHdr.Add("Content-Disposition", "inline")
+			partHdr := textproto.MIMEHeader(map[string][]string{})
+			partHdr.Add("Content-Disposition", "inline")
 
-		func() {
+			func() {
+				frameDataDecoded.mutex.Lock()
+				defer frameDataDecoded.mutex.Unlock()
+				if frameDataDecoded.ImgOrig == nil {
+					return
+				}
+				partHdr.Add("Content-Type", "image/jpeg")
+				part, err := mpw.CreatePart(partHdr)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				part.Write(frameDataDecoded.ImgOrig)
+			}()
+
+			mpw.Close()
+		})
+		http.ListenAndServe("localhost:8000", nil)
+	}()
+	a := app.New()
+	w := a.NewWindow("Output")
+	w.Resize(fyne.NewSize(640, 480))
+	window = &w
+	w.ShowAndRun()
+}
+
+func showFrames() {
+	for {
+		<-time.After(time.Duration(1) * time.Millisecond * 1000)
+		if window == nil {
+			continue
+		}
+		img := func() image.Image {
 			frameDataDecoded.mutex.Lock()
 			defer frameDataDecoded.mutex.Unlock()
-			if frameDataDecoded.ImgOrig == nil {
-				return
-			}
-			partHdr.Add("Content-Type", "image/jpeg")
-			part, err := mpw.CreatePart(partHdr)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			part.Write(frameDataDecoded.ImgOrig)
+			r := bytes.NewReader(frameDataDecoded.ImgOrig)
+			res, _ := jpeg.Decode(r)
+			return res
 		}()
-
-		mpw.Close()
-	})
-	http.ListenAndServe("localhost:8000", nil)
+		if img == nil {
+			continue
+		}
+		log.Println(img.Bounds())
+		canvImg := canvas.NewImageFromImage(img)
+		(*window).SetContent(canvImg)
+	}
 }
 
 func startWebsocketClient() {
